d94242e1c86e968c70d8effe81e1e3cc07fecbd7
Nikeuk <nikeuk@gmail.com>
2016-02-14 23:26:23 +0300

    try Kitty
    try Kitty

--- createDb.js
+++ createDb.js
@@ -1,37 +1,14 @@
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://92.168.1.38:27017/test');
 
-// Connection URL
-var url = 'mongodb://192.168.1.38:27017/chat';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+var Cat = mongoose.model('Cat', { name: String });
 
-    insertDocuments(db, function() {
+var kitty = new Cat({
+    name: 'Zildjian'
+});
+console.log(kitty);
 
-        db.close();
-    });
-
+kitty.save(function(err, kitty, affected) {
+    console.log(arguments);
 });
 
-var insertDocuments = function(db, callback) {
-    // Get the documents collection
-    var collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the document collection");
-
-        callback(result);
-    });
-    var cursor = collection.find();
-    cursor.toArray(function(err, results) {
-        console.dir(results);
-    })
-
-};
